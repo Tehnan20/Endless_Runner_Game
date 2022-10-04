d5f82adde78f2ed61564a0685594261b118034fa
Tehnan <tehnan20@gmail.com>
2022-10-04 13:56:58 +0500

    Update GameOverScript.cs

--- EndlessRunnerGame/Assets/Scripts/GameOverScript.cs
+++ EndlessRunnerGame/Assets/Scripts/GameOverScript.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class GameOverScript : MonoBehaviour
 {
     public void OnCollisionEnter(Collision col) {
-        Debug.Log("bbbb" + col.collider.tag);
         if(col.collider.tag == "Obstacle")
         {
             FindObjectOfType<ObstacleSpawnerScript>().isGameOver = false;
